b32265858956ce0b1a25961e606149269915c146
Easley <jessicaeasley@Jessicas-Air.attlocal.net>
2023-04-02 23:49:26 -0500

    Implemented Apollo Server in server.js file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
