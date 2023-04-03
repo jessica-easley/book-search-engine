771207a9a10f3affd564dc140776fe96787f1a02
Easley <jessicaeasley@Jessicas-Air.attlocal.net>
2023-04-02 23:44:44 -0500

    updated index.js and completed resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,6 +13,38 @@ const resolvers = {
     },
 
     Mutation: {
-        
-    }
-}
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            return user;
+        },
+
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AuthenticationError('Incorrect Credentials');
+        }
+
+        const rightPw = await user.isRightPassword(password);
+        if (!rightPw) {
+            throw new AuthenticationError('Incorrect Credentials');
+        }
+
+        const token = signToken(user);
+        return { token, user };
+    },
+
+    saveBook: async (parent, { input }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+                context.user._id,
+                { $addToSet: { savedBooks: input } },
+                { new: true }
+            ).populate('savedBooks');
+            return updatedUser;
+        }
+        throw new AuthenticationError('You need to be logged in!');
+    },
+    },
+};
+
+module.exports = resolvers;
